b173c448f2842648c340eab593fc55b5ec3bc4f8
Daniele Simas <83620485+danisimas@users.noreply.github.com>
2024-06-20 21:39:53 -0400

    feat: example_1 again

--- Exemplo_1_Web/build.ps1
+++
@@ -1,3 +0,0 @@
-$exclude = @("venv", "Exemplo_1_Web.zip")
-$files = Get-ChildItem -Path . -Exclude $exclude
-Compress-Archive -Path $files -DestinationPath "Exemplo_1_Web.zip" -Force
--- Exemplo_1_Web/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-zip -r "Exemplo_1_Web.zip" * -x "Exemplo_1_Web.zip"
